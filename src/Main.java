a4af7d499bfdb63ab19513d929b0e5b702752e8a
Tatiana Vasileva <tatyanushka21@mail.ru>
2022-08-30 12:04:02 +0300

    Switch int to long in sales

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String [] args) {
-        int [] sales={34200, 45000, 28600, 35800, 44800};
+        long [] sales={34200, 45000, 28600, 35800, 44800};
         SalesManager manager = new SalesManager(sales);
         System.out.println(manager.max());
     }
